baed09bee6dc12d41d4abdc389aa770b5cfdfd41
ravenusmc <mcuddy77@gmail.com>
2017-07-05 21:21:46 -0400

    setting up project structure

---
+++ main.py
@@ -0,0 +1,15 @@
+#importing outside libraries for use in the project
+from flask import Flask, session, jsonify, redirect, url_for, escape, render_template, request
+
+#Setting up Flask
+app = Flask(__name__)
+
+@app.route('/index')
+def index():
+    return render_template('index.html', title='Graph Page')
+
+
+
+#This line will actually run the app.
+if __name__ == '__main__':
+    app.run(debug=True)
